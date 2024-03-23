01be54e1a9bbc38a5447a42ec11af40bfff19889
HammadJanjua77 <160199063+Hammadjanjua77@users.noreply.github.com>
2024-03-22 18:58:12 -0700

    Update game.ts

--- game.ts
+++ game.ts
@@ -1,17 +1,22 @@
 #! /usr/bin/env node
+
 import inquirer from "inquirer";
-let randomNumber = Math.floor(Math.random() * 10 + 1);
+let randomNumber = Math.floor(Math.random() *10+1);
+
+
+
 let guessingNumber = await inquirer.prompt([
-    {
-        message: "Enter Your Number",
-        type: "number",
-        name: "YourGuess",
-    },
+  {
+    message: "Enter Your Number",
+    type: "number",
+    name: "YourGuess",
+  },
 ]);
+
 //conditional test
-if (guessingNumber.YourGuess === randomNumber) {
-    console.log("congtratulations! You Guess Is Right...........");
-}
-else {
-    console.log("Oops! You Guess Is Wrong....");
+if(guessingNumber.YourGuess === randomNumber) {
+  console.log("congtratulations! You Guess Is Right..........." );
+  
+}else{
+  console.log("Oops! You Guess Is Wrong....")
 }
