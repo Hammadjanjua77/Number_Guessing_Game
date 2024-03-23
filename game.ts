d90f516c613c80c9c55092d00597b082488ebaf3
HammadJanjua77 <160199063+Hammadjanjua77@users.noreply.github.com>
2024-03-22 18:57:21 -0700

    Update game.ts

--- game.ts
+++ game.ts
@@ -1,18 +1,17 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
-let favouriteNumber = 70;
+let randomNumber = Math.floor(Math.random() * 10 + 1);
 let guessingNumber = await inquirer.prompt([
-  {
-    message: "Enter Your Number",
-    type: "number",
-    name: "YourGuess",
-  },
+    {
+        message: "Enter Your Number",
+        type: "number",
+        name: "YourGuess",
+    },
 ]);
-if (guessingNumber.YourGuess < 70) {
-  console.log("opp's! your Number is too Low ");
-} else if (guessingNumber.YourGuess > 70) {
-  console.log("opp's! your Number is too High ");
-} else if (guessingNumber.YourGuess === 70) {
-  console.log("Congratulation! You Guess Correct Number ");
-} else {
-  console.log("Try Again !!!!!!!!!!");
+//conditional test
+if (guessingNumber.YourGuess === randomNumber) {
+    console.log("congtratulations! You Guess Is Right...........");
+}
+else {
+    console.log("Oops! You Guess Is Wrong....");
 }
